f41bba2f37ba627541f36fa8a25bf932175552c0
Jonathan Zrake <jonathan.zrake@gmail.com>
2012-04-26 17:23:17 -0400

    ww2.py works for words of any length, but past path lengths of about 9 it gets way too slow. It always finds all of the shortest paths when it does find one.

--- ww2.py
+++ ww2.py
@@ -3,6 +3,7 @@
 import sys
 import itertools
 
+
 def collect_words_of_length(size, loc='/usr/share/dict/words'):
     """
     Return a list of dictionary words having length `size`.
@@ -14,30 +15,47 @@ def collect_words_of_length(size, loc='/usr/share/dict/words'):
     return [w.strip() for w in inputDict if len(w.strip()) == size]
 
 
-def walk_from(start, dest):
-    print "attempting to walk from %s to %s" % (start, dest)
+def walk_from(start, dest, max_depth=4):
+    """
+    Finds all shortest paths between words `start` and `dest`. Only generates
+    paths of odd length.
+    """
     if len(start) != len(dest):
         print "words must be of equal length", start, dest
         return None
 
     space = collect_words_of_length(len(start))
 
-    def one_away_from(word):
-        return [w for w in space if sum([a != b for a,b in zip(w, word)]) == 1]
+    def append_one_aways(seq):
+        """
+        `seq` is a list of lists: [['dog', 'dag'], ['dog', 'bog'], ...]
+        """
+        new_seq = [ ]
+        for s in seq:
+            new_seq += [s + [w] for w in space if
+                        sum([a != b for a,b in zip(w, s[-1])]) == 1]
+        return new_seq
 
-    gen1L = one_away_from(start)
-    gen1R = one_away_from(dest)
+    gL = [[start]]
+    gR = [[dest]]
 
-    gen2L = [{'seed': w, 'stem': one_away_from(w)} for w in gen1L]
-    gen2R = [{'seed': w, 'stem': one_away_from(w)} for w in gen1R]
+    depth = 0
+    npaths = 0
 
-    intermed = set()
+    while npaths == 0 and depth < max_depth:
+        print "trying depth", depth
+        gL = append_one_aways(gL)
+        gR = append_one_aways(gR)
 
-    for gL, gR in itertools.product(gen2L, gen2R):
-        overlap = set(gL['stem']) & set(gR['stem'])
-        if overlap:
-            for o in overlap:
-                print [start, gL['seed'], o, gR['seed'], dest]
+        for L, R in itertools.product(gL, gR):
+            if L[-1] == R[-1]:
+                path = L + list(reversed(R))[1:]
+                npaths += 1
+                print ' -> '.join(path)
+        depth += 1
+
+    print ("found %d paths of length %d from %s to %s" %
+           (npaths, 2*depth + 1, start, dest))
 
 
 if __name__ == "__main__":
