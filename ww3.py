4df18580a1924fb72d5799a6e201a6912e0bbb06
Jonathan Zrake <jonathan.zrake@gmail.com>
2012-04-27 11:30:55 -0400

    walking walking

--- ww3.py
+++ ww3.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 
+
 class TreeNode:
     def __init__(self, value, last=None):
         self._value = value
@@ -24,7 +25,8 @@ class TreeNode:
         return [self] + (self._last.nlineage() if self._last else [ ])
     def head(self):
         return self.nlineage()[-1]
-
+    def values(self):
+        return [x._value for x in self._next]
 
 def test_tree():
     head = TreeNode("head")
@@ -57,6 +59,49 @@ def collect_words_of_length(size, loc='/usr/share/dict/words'):
     return [w.strip() for w in inputDict if len(w.strip()) == size]
 
 
+def get_neighbors(word, candidates, exclude=[], pred=list):
+    return pred([w for w in candidates if metric(word, w) == 1 and
+                 w not in exclude])
+
+
+def test_get_neighbors():
+    allwords = collect_words_of_length(4)
+    print get_neighbors("barf", allwords, exclude=["barn", "zarf"])
+
+
+def walk_from(start, dest):
+
+    if len(start) != len(dest):
+        print "words must be of equal length", start, dest
+        return None
+
+    space = collect_words_of_length(len(start))
+
+    if start not in space:
+        print start, "is not in the dictionary"
+        return
+    if dest not in space:
+        print dest, "is not in the dictionary"
+        return
+
+    walkerL = TreeNode(start)
+    walkerR = TreeNode(dest)
+
+    for word in get_neighbors(walkerL._value, space):
+        walkerL.add_node(word)
+
+    for word in get_neighbors(walkerR._value, space):
+        walkerR.add_node(word)
+
+    candL = sorted(walkerL.values(), key=lambda x: metric(x, walkerR._value))
+
+    print candL, [metric(c, walkerR._value) for c in candL]
+
+
 if __name__ == "__main__":
-    test_tree()
-    test_metric()
+    if False:
+        test_tree()
+        test_metric()
+        test_get_neighbors()
+    walk_from("book", "nook")
+
